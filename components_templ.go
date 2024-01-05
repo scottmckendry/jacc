4b7d9d4109a9758a8b68f58537f69b70e8bad7f0
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2024-01-06 08:53:59 +1300

    add default theme

--- components_templ.go
+++ components_templ.go
@@ -41,7 +41,7 @@ func Index(config config.Config) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(fmt.Sprintf("public/%s.css", config.Theme)))
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(fmt.Sprintf("public/css/%s.css", config.Theme)))
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
